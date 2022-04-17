cf8698df170c1824a376890eee32ce6153ca4206
Vladislav <axperien@yandex.ru>
2022-04-17 14:01:38 +0400

    Начальный коммит

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'hbs');
+
+app.use(express.static(`${__dirname}/dist/`));
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './scss/main.scss';
+
+import modal from './components/modal/modal';
+import fileinput from './components/fileinput/fileinput';
+
+modal();
+fileinput();
