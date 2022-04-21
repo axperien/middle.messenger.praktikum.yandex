4394f2bb26ba6f6cea6404756b5614111e7e65b9
Vladislav <axperien@yandex.ru>
2022-04-21 10:19:20 +0400

    Проверка на существование DOM-элемента

--- src/templates/pages/login/login.js
+++ src/templates/pages/login/login.js
@@ -2,13 +2,17 @@ export default () => {
     const form = document.querySelector('.form');
     const formButton = document.querySelector('.form__button');
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-    });
+    if (form !== null) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+        });
+    }    
 
     // редирект пока что без отправки формы
-    formButton.addEventListener('click', (e) => {
-        e.preventDefault();
-        window.location = 'chats';
-    });
+    if (formButton !== null) {
+        formButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            window.location = 'chats';
+        });
+    }    
 }
