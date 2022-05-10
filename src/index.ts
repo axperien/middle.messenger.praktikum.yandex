711b46cf879a55a400ca9cb5c562e1732e480cbf
Vladislav <axperien@yandex.ru>
2022-05-10 15:30:22 +0400

    Ручная регистрация компонентов

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,32 @@ import './scss/main.scss';
 import './core/registerHelpers';
 import registerComponent from './core/registerComponents';
 import { getCurrentPage } from './core/routing';
-import Block from './core/Block';
 
-/* eslint-disable global-require */
-const components = require('./components/**/index.ts') as {[key: string]: { default: typeof Block }};
+import { Button } from './components/button/button';
+import { Field } from './components/field/field';
+import { Form } from './components/form/form';
+import { Link } from './components/link/link';
+import { Error } from './components/error/error';
+import { BackUrl } from './components/backUrl/backUrl';
+import { Avatar } from './components/avatar/avatar';
+import { UserField } from './components/userField/userField';
+import { ChatList } from './components/chatList/chatList';
+import { ChatInfo } from './components/chatInfo/chatInfo';
+import { ChatForm } from './components/chatForm/chatForm';
+import { ChatMessage } from './components/chatMessage/chatMessage';
 
-Object.values(components).forEach((component) => {
-    registerComponent(component.default);
-});
+registerComponent(Button);
+registerComponent(Field);
+registerComponent(Form);
+registerComponent(Link);
+registerComponent(Error);
+registerComponent(BackUrl);
+registerComponent(Avatar);
+registerComponent(UserField);
+registerComponent(ChatList);
+registerComponent(ChatInfo);
+registerComponent(ChatForm);
+registerComponent(ChatMessage);
 
 const setDocumentTitle = (title: string): void => {
     if (title) {
